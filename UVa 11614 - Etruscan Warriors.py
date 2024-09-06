83cc72804f884620855477366b792d21120db839
Pablo Vinicius <128602954+PabloVLS@users.noreply.github.com>
2024-09-06 19:03:34 -0300

    Update UVa 11614 - Etruscan Warriors.py

--- UVa 11614 - Etruscan Warriors.py
+++ UVa 11614 - Etruscan Warriors.py
@@ -1,16 +1,19 @@
-import  sys
-from msvcrt import kbhit
-
+import math
 n=int(input())
 
 
 for _ in range(n):
-    linha = 0
     soldados = int(input())
-    k=1
-    while soldados >= k:
-        soldados-=k
-        k+=1
-        linha+=1
+    k= int((-1 + math.sqrt(1+8*soldados))//2)
 
-    print(linha)
+    print(k)
+
+
+# Calculando o número de fileiras (k) com base no número de guerreiros (n)
+# Passo 1: Multiplicamos 8 pelo número de guerreiros
+# Passo 2: Somamos 1 ao resultado anterior
+# Passo 3: Calculamos a raiz quadrada dessa soma
+# Passo 4: Subtraímos 1 do resultado da raiz
+# Passo 5: Dividimos o resultado final por 2 para obter o valor de k
+
+#k = (-1 + RAIZ(1 + 8 * n)) / 2
